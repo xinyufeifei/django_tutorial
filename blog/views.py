e4c810d604ea4879adf8ba9b9f0213c82c0e9164
Qiong Zeng <xinyufeifei@gmail.com>
2019-12-21 15:56:57 -0600

    update the template of the app.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        "author": "Qiong",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "Dec 21, 2019",
+    },
+    {
+        "author": "Jane Don",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "Dec 22, 2019",
+    },
+]
 
 # Create your views here.
+def home(request):
+    context = {"posts": posts}
+    return render(request, "blog/home.html", context)
+
+
+def about(request):
+    return render(request, "blog/about.html", {"title": "About"})
