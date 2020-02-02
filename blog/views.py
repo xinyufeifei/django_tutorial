1c1cf945b9bb66d68f380a0161e4ab8700fa25a7
Qiong Zeng <xinyufeifei@gmail.com>
2020-02-02 14:33:45 -0600

    part 11 pagination fix typos.

--- blog/views.py
+++ blog/views.py
@@ -31,8 +31,8 @@ class UserPostListView(ListView):
     context_object_name = "posts"
     paginate_by = 5
 
-    def get_query_set(self):
-        user = get_object_or_404(User, username=self.kwargs.get('username'))
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
         return Post.objects.filter(author=user).order_by("-date_posted")
 
 
